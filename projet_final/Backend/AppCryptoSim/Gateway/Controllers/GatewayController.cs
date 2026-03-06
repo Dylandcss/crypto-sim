916e74d0e7d27496ca1d62370e2a9f021e559bdd
helene <helenevercaygne@hotmail.fr>
2026-03-06 16:30:06 +0100

    dev gateway

---
+++ projet_final/Backend/AppCryptoSim/Gateway/Controllers/GatewayController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace Gateway.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GatewayController : ControllerBase
+{
+
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public GatewayController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    private void _AddAuthTokenToHeader(HttpClient client)
+    {
+        var authHeader = HttpContext.Request.Headers["Authorization"].FirstOrDefault();
+        if (authHeader != null && authHeader.StartsWith("Bearer "))
+        {
+            var token = authHeader.Substring("Bearer ".Length).Trim();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+    }
+
+    [HttpPost("/api/auth/login")]
+    public async Task<IActionResult> Login([FromBody] object body)
+    {
+        var client = _httpClientFactory.CreateClient("AuthClient");
+
+        var jsonContent = new StringContent(
+            System.Text.Json.JsonSerializer.Serialize(body),
+            System.Text.Encoding.UTF8,
+            "application/json"
+        );
+
+        var response = await client.PostAsync("/api/auth/login", jsonContent);
+        var content = await response.Content.ReadAsStringAsync();
+
+        return StatusCode((int)response.StatusCode, content);
+    }
+
+
+    [HttpGet("/api/market")]
+    public async Task<IActionResult> GetMarket()
+    {
+        var client = _httpClientFactory.CreateClient("MarketClient");
+
+        var response = await client.GetAsync("/api/market");
+        var content = await response.Content.ReadAsStringAsync();
+
+        return Content(content, "application/json", System.Text.Encoding.UTF8);
+    }
+
+    [HttpGet("/api/order")]
+    [Authorize]
+    public async Task<IActionResult> GetOrder()
+    {
+        var client = _httpClientFactory.CreateClient("OrderClient");
+
+        _AddAuthTokenToHeader(client);
+
+        var response = await client.GetAsync("/api/order");
+        var content = await response.Content.ReadAsStringAsync();
+
+        return Content(content, "application/json", System.Text.Encoding.UTF8);
+    }
+
+    [HttpPost("/api/portfolio")]
+    [Authorize]
+    public async Task<IActionResult> CreatePortfolio([FromBody] object body)
+    {
+        var client = _httpClientFactory.CreateClient("PortfolioClient");
+        _AddAuthTokenToHeader(client);
+
+        var jsonContent = new StringContent(
+            System.Text.Json.JsonSerializer.Serialize(body),
+            System.Text.Encoding.UTF8,
+            "application/json"
+        );
+
+        var response = await client.PostAsync("/api/portfolio", jsonContent);
+        var content = await response.Content.ReadAsStringAsync();
+
+        return StatusCode((int)response.StatusCode, content);
+    }
+
+}
--- projet_final/Backend/AppCryptoSim/Gateway/Program.cs
+++ projet_final/Backend/AppCryptoSim/Gateway/Program.cs
@@ -1,13 +1,58 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using DotNetEnv;
 
-// Add services to the container.
+Env.Load("../.env");
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddEnvironmentVariables();
+
+
+// Charger le fichier .env situé à la racine de la solution
+
+
+// Accéder aux variables comme si elles étaient dans appsettings.json
+
+//var secretKey = builder.Configuration["JWT_SECRET"]
+                //?? throw new InvalidOperationException("JWT_SECRET manquant.");
+
+string issuer = Environment.GetEnvironmentVariable("JWT_ISSUER");
+string audience = Environment.GetEnvironmentVariable("JWT_AUDIENCE");
+string secret = Environment.GetEnvironmentVariable("JWT_SECRET");
+var secretKey = Encoding.UTF8.GetBytes(secret);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = issuer,
+            ValidateAudience = true,
+            ValidAudience = audience,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(secretKey),
+            ValidateLifetime = true
+        };
+    });
+
+// Config des HttpClient
+builder.Services.AddHttpClient("AuthClient", client => client.BaseAddress = new Uri("http://localhost:5001"));
+
+builder.Services.AddHttpClient("MarketClient", client => client.BaseAddress = new Uri("http://localhost:5002"));
+
+builder.Services.AddHttpClient("OrderClient", client => client.BaseAddress = new Uri("http://localhost:5003"));
+
+builder.Services.AddHttpClient("PortfolioClient", client => client.BaseAddress = new Uri("http://localhost:5003"));
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
