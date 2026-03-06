2b817d95076a638d253ca37ff9edbe10e1b848b2
helene <helenevercaygne@hotmail.fr>
2026-03-06 11:09:59 +0100

    feat(PortfolioService): ajout des modèles et DTOs

---
+++ projet_final/Backend/AppCryptoSim/PortfolioService/DTOs/HoldingDetail.cs
@@ -0,0 +1,14 @@
+﻿namespace PortfolioService.DTOs
+{
+    public record HoldingDetail(
+    string Symbol,
+    string Name,
+    decimal Quantity,
+    decimal AverageBuyPrice,
+    decimal CurrentPrice,
+    decimal CurrentValue,
+    decimal GainLoss,
+    decimal GainLossPercent
+);
+
+}
---
+++ projet_final/Backend/AppCryptoSim/PortfolioService/DTOs/PortfolioSummary.cs
@@ -0,0 +1,12 @@
+﻿namespace PortfolioService.DTOs
+{
+    public record PortfolioSummary(
+     int UserId,
+     decimal CashBalance,
+     decimal TotalInvested,
+     decimal TotalCurrentValue,
+     decimal TotalGainLoss,
+     decimal TotalGainLossPercent,
+     List<HoldingDetail> Holdings
+ );
+}
