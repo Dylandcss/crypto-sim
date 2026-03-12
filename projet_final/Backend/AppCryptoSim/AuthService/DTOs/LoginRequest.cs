28a32fcb216c43e433b5a597eea4f0b6c503d191
safina3d <safina3d@users.noreply.github.com>
2026-03-12 22:51:31 +0100

    fix(auth): harmonise les DTOs de login et register

--- projet_final/Backend/AppCryptoSim/AuthService/DTOs/LoginRequest.cs
+++ projet_final/Backend/AppCryptoSim/AuthService/DTOs/LoginRequest.cs
@@ -2,7 +2,7 @@
 
 namespace AuthService.Dtos;
 
-public record LoginRequest(
-    [Required] string Username,
-    [Required] string Password
+public record LoginRequest (
+    [Required][MinLength(3, ErrorMessage = "Le nom d'utilisateur doit contenir au moins 3 caractères.")] string Username,
+    [Required][MinLength(6, ErrorMessage = "Le mot de passe doit contenir au moins 6 caractères.")] string Password
 );
